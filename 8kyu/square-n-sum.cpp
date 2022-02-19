33221d6af3814c9b482a355d6b56f807bb2d7a35
Yessica Gaona <yessicagaonam@gmail.com>
2022-02-19 14:48:12 -0600

    codewars practice

---
+++ 8kyu/square-n-sum.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+int square_sum(const std::vector<int>& numbers)
+{
+  int sum = 0;
+  
+  for (int i=0; i<numbers.size(); i++){
+     sum += numbers[i] * numbers[i];
+  }
+  
+  return sum;
+}
